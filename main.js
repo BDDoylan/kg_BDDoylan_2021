390b8f7e667030d6dc4cd3ce97d8dd419662c921
Doylan <bddoylan@gmail.com>
2021-04-27 13:00:46 -0400

    Implementing the converter function

--- main.js
+++ main.js
@@ -2,3 +2,19 @@
     Author: Doylan Mihov
     Date: 04/27/2021
 */
+
+let phoneticNumberStorage = ["Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"];
+
+const converter = array => {
+    let output = [];
+    for(let i = 0; i < array.length; ++i){
+        let stringOutput = "";
+        let numString = array[i].toString();
+        for(let j = 0; j < numString.length; ++j){
+            let stringNum = Number(numString[j]);
+            stringOutput += phoneticNumberStorage[stringNum];
+        }
+        output.push(stringOutput);
+    }
+    return output;
+}
